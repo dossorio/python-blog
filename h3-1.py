00eab0c8ee19025707ce68cbd85e9ab5953fe2d0
Daniel Ossorio <daniel@ossorio.me>
2016-04-05 08:27:07 +0100

    h3-1

--- h3-1.py
+++ h3-1.py
@@ -6,11 +6,27 @@ db = connection.school
 students = db.students
 
 for student in students.find():
-    scores = student["scores"]
-    for index, score in enumerate(scores):
-        if ()
-        print(index, score)
 
-        break
-    break
+    scores = student['scores']
+    lowestHWScore = None
+
+    # print(scores)
+
+    for index, score in enumerate(scores):
+
+        if score['type'] == 'homework':
+
+            if lowestHWScore is None or scores[index]['score'] < scores[lowestHWScore]['score']:
+                lowestHWScore = index
+
+    scores.pop(lowestHWScore)
+
+    student['scores'] = scores
+    students.save(student)
+    print('Saved: ' + str(student['_id']))
+
+    # print(scores)
+    # print('======================')
+
+
 
